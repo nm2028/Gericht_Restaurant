0c045e27ff0b48d1caaa16625fa01d234e2219a5
Nikhil Mishra <59688833+nm2028@users.noreply.github.com>
2023-09-26 15:56:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Navbar } from './components';
+import AboutUs from './container/AboutUs/AboutUs';
+import Chef from './container/Chef/Chef';
+import FindUs from './container/Findus/FindUs';
+import Footer from './container/Footer/Footer';
+import Gallery from './container/Gallery/Gallery';
+import Header from './container/Header/Header';
+import Intro from './container/Intro/Intro';
+import Laurels from './container/Laurels/Laurels';
+import SpecialMenu from './container/Menu/SpecialMenu';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
